3ea801cf3127cdc3f3c19863ebec57ee3c8e4e6e
Lezoray <olivier.lezoray@gmail.com>
2024-01-18 18:32:57 +0100

    Ajout des fichiers

---
+++ insurance_app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pandas as pd 
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open('insurance.pickle',"rb"));
+cols = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
+
+@app.route('/')
+def home():
+    return render_template("input.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    features = [x for x in request.form.values()]
+    final = np.array(features)
+    data_unseen = pd.DataFrame([final], columns = cols)
+    prediction=model.predict(data_unseen)
+    result=prediction[0]
+    return render_template('input.html',resultat=f"Le cout annuel de l'assurance est de {result:.2f}US$")
+
+if __name__ == '__main__':
+    app.run(debug=True)
